dfcc13f6f4a7e8fee0475830182eec9fe4a8c8c0
triptocode <3580rio@gmail.com>
2022-06-01 23:34:36 +0900

    fun main(){ helloWorld() } fun helloWorld() : Unit {     println(" ")

--- app/src/main/java/com/example/kotlinstudy01/study220531.kt
+++ app/src/main/java/com/example/kotlinstudy01/study220531.kt
@@ -7,11 +7,9 @@ fun main(){
 // 1. 함수
 // 자바는 리턴형 void, int 등을 앞에 적어주었다면
 // 코틀린은 항상 fun 을 적어주고 리턴형이 무엇이든 Unit을 적거나 생략가능
-
-// 아래의 코틀린은 파라미터가 없어서 (), 리턴형도 없어서 {}
-// fun helloWolrd(){ } 이렇게 적거나 아래와같이도 작성가능
-
 fun helloWorld() : Unit {
-
     println("Welcome to Kotlin")
 }
+// 위 코틀린코드에서 파라미터가 없어서 (), 리턴형도 없어서 {} 으로
+// fun helloWolrd(){} 이렇게 작성하거나 fun helloWorld(): Unit {} 도 작성가능
+
