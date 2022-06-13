7bcaeb88b16f496dae9fca8e71aca7c94c032330
triptocode <3580rio@gmail.com>
2022-06-14 06:05:24 +0900

    TypeCasting: var y: Int = 2 // y.toLong()

---
+++ app/src/main/java/com/example/kotlinstudy01/study220614_typeCasting.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinstudy01
+
+
+fun main() {
+    var x: Int = 1
+    var b: Long = x
+
+    var y: Int = 2
+    var c: Long = y.toLong()
+
+    println(x)
+    println(y)
+
+}
