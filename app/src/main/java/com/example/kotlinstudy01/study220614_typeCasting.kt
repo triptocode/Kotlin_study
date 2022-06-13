bd0c836c290518c04b630a110ff2cc1a0eea1372
triptocode <3580rio@gmail.com>
2022-06-14 06:27:39 +0900

    TypeCasting: var y: Int = 2 // y.toLong()

--- app/src/main/java/com/example/kotlinstudy01/study220614_typeCasting.kt
+++ app/src/main/java/com/example/kotlinstudy01/study220614_typeCasting.kt
@@ -3,10 +3,10 @@ package com.example.kotlinstudy01
 
 fun main() {
     var x: Int = 1
-    var b: Long = x
+    var b: Long = x  // 타입이 다른경우 값을 할당할수 없다
 
     var y: Int = 2
-    var c: Long = y.toLong()
+    var c: Long = y.toLong()   // 타입이 달라서 int였던 y를 Long으로 type casting
 
     println(x)
     println(y)
